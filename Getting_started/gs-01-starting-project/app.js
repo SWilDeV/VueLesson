2367925adc953902d76b91f9c37175e617381b03
sean wilson <sean0669@gmail.com>
2021-01-14 23:11:49 -0500

    first vue app

---
+++ Getting_started/gs-01-starting-project/app.js
@@ -0,0 +1,32 @@
+
+Vue.createApp({
+    data(){
+        return{
+            goals:[],
+            enteredValue: ''
+        };
+    },
+    methods:{
+        addGoal(){
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
+
+
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+
+// buttonEl.addEventListener('click',addGoal);
+
+
+// function addGoal(){
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
